7cf1dbfffd57ab88c1de95717ed007acd0d8c68b
Matthew McGregor <mmcgregor@hawk.iit.edu>
2022-02-04 00:12:05 -0600

    Initial Commit

---
+++ Shared/Homework2problem2App.swift
@@ -0,0 +1,17 @@
+//
+//  Homework2problem2App.swift
+//  Shared
+//
+//  Created by MatthewMcGregor on 2/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Homework2problem2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
